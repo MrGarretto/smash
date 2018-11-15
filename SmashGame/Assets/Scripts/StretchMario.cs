5dc80f849af5255f07d06cef3cf60cb7caca3c34
nyxad <30995260+nyxad@users.noreply.github.com>
2018-11-14 19:09:15 -0500

    Added models & some random code

---
+++ SmashGame/Assets/Scripts/StretchMario.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StretchMario : MonoBehaviour {
+
+    Rigidbody marioRB;
+
+	// Use this for initialization
+	void Start () {
+        marioRB = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //marioRB.rotation = Random.rotation;
+        transform.position.Set(transform.position.x, transform.position.y + 1f, transform.position.z);
+	}
+}
